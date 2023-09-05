a6d8f55f689e2cf5e72fa502811eb35de605a3b0
Alexey Babikov <alex94.73rus@gmail.com>
2023-09-05 14:21:00 +0400

    checked test

--- locators.py
+++ locators.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.by import By
 class Main:
     PYTHON_PAGE1 = (By.XPATH, "//a[normalize-space()='1. Installation']")
     PYTHON_PAGE2 = (By.XPATH, "//a[normalize-space()='1.2. Installing Python bindings for Selenium']")
-    PYTHON_PAGE3 = (By.XPATH, "//a[normalize-space()='7. WebDriver AP1']")
+    PYTHON_PAGE3 = (By.XPATH, "//a[normalize-space()='7. WebDriver API']")
