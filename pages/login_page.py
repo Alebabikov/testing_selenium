2496bef747e43e0c527aa6cb51d2140b9e63f1b3
Alexey Babikov <alex94.73rus@gmail.com>
2023-09-05 09:58:01 +0400

    check

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,7 +8,7 @@ class PythonSite(BasePage):
     def test_click_title(self):
         click_link = self.driver.find_element(*locators.Main.PYTHON_PAGE1)
         click_link.click()
-        time.sleep(10)
+        time.sleep(12)
         click_link = self.driver.find_element(*locators.Main.PYTHON_PAGE2)
         click_link.click()
         time.sleep(10)
